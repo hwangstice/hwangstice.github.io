5ee6e7a3fe361cdc2e0357c0966039208db2f255
Dat Hoang <datquochoang0587@gmail.com>
2025-03-21 22:36:31 +0700

    update page link

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
 export const USER_NAME = "Hwangstice";
-export const USER_SITE = "https://frosti.saroprock.com"; // At the same time, this is also the site retrieved by the i18n configuration.
+export const USER_SITE = "https://dathwang.github.io/"; // At the same time, this is also the site retrieved by the i18n configuration.
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
